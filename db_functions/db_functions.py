960e04c0e1733f9e732bc6d05989a68f7b522f76
Alex Bloom <lehanbloom@gmail.com>
2020-05-20 16:11:13 +0300

    Added a db_functions class and created the basic db functionalities: add student, delete student, get all students, get one student.

--- db_functions/db_functions.py
+++ db_functions/db_functions.py
@@ -1,7 +1,38 @@
-from pymongo import MongoClient
+from initialize_db import db
+from bson import ObjectId
 
 class DbFunctions:
 
+    def add_student(self, student):
+        student_id = db.students.insert(student)
+        return student_id
+
+    def get_all_students(self):
+        students = db.students.find({})
+        for i in students:
+            print(i)
+        return students
+
+    def get_single_student(self, student_id):
+        student = db.students.find_one({'_id': ObjectId(student_id)})
+        return student
+
+    def delete_student(self, student_id):
+        student = db.students.delete_one({'_id': ObjectId(student_id)})
+        if student.acknowledged and student.deleted_count == 1:
+            return True
+        else:
+            return False
 
 
+
+
+
+test = DbFunctions()
+
+# for i in range(1,10):
+#     test.add_student({"name": "Rachel" + str(i)})
+# test.get_all_students()
+# test.delete_student('5ec52776f7e87904cd20c3db')
+# test.get_all_students()
 
--- models/student.py
+++ models/student.py
@@ -6,9 +6,8 @@ class Student:
     def to_json(self):
         return json.dumps(self, default=lambda o: o.__dict__)
 
-    def __init__(self, student_id, first_name, last_name, create_time, last_updated_time, current_magic_skills,
+    def __init__(self, first_name, last_name, create_time, last_updated_time, current_magic_skills,
                  desired_magic_skills, course_interests):
-        self.student_id = str(student_id)
         self.first_name = first_name
         self.last_name = last_name
         self.create_time = str(create_time)
