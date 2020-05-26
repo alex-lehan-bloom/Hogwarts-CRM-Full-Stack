a00ab7f558f24521ebffd43ffc9539f4255c6a15
Alex Bloom <lehanbloom@gmail.com>
2020-05-26 11:54:26 +0300

    Added side menu to Hogwarts CRM.

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,11 +1,18 @@
-import React, { useEffect } from "react";
-import "./App.css";
+import React from "react";
 import Home from "./components/Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
--- front-end/src/components/SideMenu.jsx
+++ front-end/src/components/SideMenu.jsx
@@ -1,10 +1,36 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import { Button } from "@material-ui/core";
 import "../css/SideMenu.css";
 
 function SideMenu(props) {
   return (
     <div className="sidemenu">
-      <p>Test</p>
+      <div className="divider" />
+      <NavLink
+        exact
+        to="/"
+        className="navbar-link"
+        activeClassName="navbar-link-active"
+      >
+        Dashboard
+      </NavLink>
+      <NavLink
+        exact
+        to="/students"
+        className="navbar-link"
+        activeClassName="navbar-link-active"
+      >
+        Students
+      </NavLink>
+      <NavLink
+        exact
+        to="/add-student"
+        className="navbar-link"
+        activeClassName="navbar-link-active"
+      >
+        Add Student
+      </NavLink>
     </div>
   );
 }
