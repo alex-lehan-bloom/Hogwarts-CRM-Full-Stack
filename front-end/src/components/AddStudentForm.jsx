bc7efce31f5a74b8dddc2546c50ada0907ba7434
Alex Bloom <lehanbloom@gmail.com>
2020-05-26 21:32:14 +0300

    Started on material UI form for enrolling a new student into the school.

--- front-end/src/components/AddStudentForm.jsx
+++ front-end/src/components/AddStudentForm.jsx
@@ -1,101 +1,85 @@
-import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import FormLabel from "@material-ui/core/FormLabel";
-import FormControl from "@material-ui/core/FormControl";
-import FormGroup from "@material-ui/core/FormGroup";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
+import React, { useState, useEffect } from "react";
+import TextField from "@material-ui/core/TextField";
+import InputLabel from "@material-ui/core/InputLabel";
+import MenuItem from "@material-ui/core/MenuItem";
 import FormHelperText from "@material-ui/core/FormHelperText";
-import Checkbox from "@material-ui/core/Checkbox";
+import FormControl from "@material-ui/core/FormControl";
+import { makeStyles } from "@material-ui/core/styles";
+
+import Select from "@material-ui/core/Select";
+import "../css/AddStudentForm.css";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    display: "flex",
-  },
-  formControl: {
-    margin: theme.spacing(3),
+    marginLeft: 50,
+    "& .MuiTextField-root": {
+      width: 300,
+    },
+    "& .MuiInputBase-root": {
+      width: 300,
+    },
   },
 }));
 
-function StudentForm() {
+function AddStudentForm() {
   const classes = useStyles();
-  const [state, setState] = React.useState({
-    gilad: true,
-    jason: false,
-    antoine: false,
-  });
+  let [firstName, setFirstName] = useState();
+  let [lastName, setLastName] = useState();
+  let [house, setHouse] = useState("");
+  let houses = ["Gryfindor", "Hufflepuff", "Ravenclaw", "Slytherin"];
 
-  const handleChange = (event) => {
-    setState({ ...state, [event.target.name]: event.target.checked });
-  };
+  function handleFirstNameChange(event) {
+    setFirstName(event.target.value);
+  }
+  function handleLastNameChange(event) {
+    setLastName(event.target.value);
+  }
+  function handleHouse(event) {
+    setHouse(event.target.value);
+  }
 
-  const { gilad, jason, antoine } = state;
-  const error = [gilad, jason, antoine].filter((v) => v).length !== 2;
+  //   useEffect(() => {
+  //     console.log(house);
+  //   }, [house]);
 
   return (
-    <div className={classes.root}>
-      <FormControl component="fieldset" className={classes.formControl}>
-        <FormLabel component="legend">Assign responsibility</FormLabel>
-        <FormGroup>
-          <FormControlLabel
-            control={
-              <Checkbox checked={gilad} onChange={handleChange} name="gilad" />
-            }
-            label="Gilad Gray"
-          />
-          <FormControlLabel
-            control={
-              <Checkbox checked={jason} onChange={handleChange} name="jason" />
-            }
-            label="Jason Killian"
-          />
-          <FormControlLabel
-            control={
-              <Checkbox
-                checked={antoine}
-                onChange={handleChange}
-                name="antoine"
-              />
-            }
-            label="Antoine Llorca"
-          />
-        </FormGroup>
-        <FormHelperText>Be careful</FormHelperText>
-      </FormControl>
-      <FormControl
-        required
-        error={error}
-        component="fieldset"
-        className={classes.formControl}
-      >
-        <FormLabel component="legend">Pick two</FormLabel>
-        <FormGroup>
-          <FormControlLabel
-            control={
-              <Checkbox checked={gilad} onChange={handleChange} name="gilad" />
-            }
-            label="Gilad Gray"
-          />
-          <FormControlLabel
-            control={
-              <Checkbox checked={jason} onChange={handleChange} name="jason" />
-            }
-            label="Jason Killian"
-          />
-          <FormControlLabel
-            control={
-              <Checkbox
-                checked={antoine}
-                onChange={handleChange}
-                name="antoine"
-              />
-            }
-            label="Antoine Llorca"
-          />
-        </FormGroup>
-        <FormHelperText>You can display an error</FormHelperText>
-      </FormControl>
-    </div>
+    <FormControl className={classes.root}>
+      <div className="first-name">
+        <TextField
+          onChange={(event) => {
+            handleFirstNameChange(event);
+          }}
+          label="First Name"
+          variant="outlined"
+        />
+      </div>
+      <div className="last-name">
+        <TextField
+          onChange={(event) => {
+            handleLastNameChange(event);
+          }}
+          label="Last Name"
+          variant="outlined"
+        />
+      </div>
+      <div>
+        <p>House</p>
+        <Select
+          //   className={classes.house}
+          value={house}
+          onChange={(event) => {
+            handleHouse(event);
+          }}
+        >
+          <MenuItem value={house}></MenuItem>
+          <MenuItem value={10}>{houses[0]}</MenuItem>
+          <MenuItem value={20}>{houses[1]}</MenuItem>
+          <MenuItem value={30}>{houses[2]}</MenuItem>
+          <MenuItem value={30}>{houses[3]}</MenuItem>
+        </Select>
+      </div>
+    </FormControl>
   );
 }
 
-export default StudentForm;
+export default AddStudentForm;
